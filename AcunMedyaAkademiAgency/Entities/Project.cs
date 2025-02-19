2ccb67adde6cd0ce4eb63bd0e2b20498379d305c
Büşra Gümüşay <busragumusay2@gmail.com>
2025-02-19 18:55:47 +0300

    Add project files.

---
+++ AcunMedyaAkademiAgency/Entities/Project.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AcunMedyaAkademiAgency.Entities
+{
+	public class Project
+	{
+		public int ProjectId { get; set; }
+		public string ImageUrl { get; set; }
+		public string Title { get; set; }
+    }
+}
